6db418b72de90e20f92ffb64ea600ef129bbaec3
Ankit Khatry <154233487+ankitkhatry@users.noreply.github.com>
2025-02-13 06:18:14 -0800

    Update connection.php

--- HOSTELMS/connection.php
+++ HOSTELMS/connection.php
@@ -4,10 +4,8 @@ $username = "root";
 $password = "";
 $database = "hostel";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
